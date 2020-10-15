9f4e21997c4062c9e7a8b78760c886b967cef803
hieunugent <hieunugent@gmail.com>
2020-10-15 01:08:52 -0700

    add the data

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
-import { FormControl, MenuItem } from '@material-ui/core';
+import { Card, FormControl, MenuItem } from "@material-ui/core";
 import Select from "@material-ui/core/Select";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import './App.css';
-import InfoBox from './InfoBox';
-import Map from './Map';
+import "./App.css";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('worldwide');
+  const [country, setCountry] = useState("worldwide");
+  const[countryInfo, setCountryInfo]= useState({});
   useEffect(()=>{
-        // send a req to server and do sth with it
-        const getCountriesData = async ()=> {
-          await fetch("https://disease.sh/v3/covid-19/countries")
-          .then((response)=> response.json())
-          .then((data)=> {
-            const countries = data.map((country)=> ({
-              name:country.country,
-              value:country.countryInfo.iso2,
-            }));
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(res => res.json())
+    .then((data) => {
+      setCountryInfo(data);
+    });
+  })
+  useEffect(() => {
+    // send a req to server and do sth with it
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
 
+          setCountries(countries);
+        });
+    };
 
-            setCountries(countries)
-          });
-        };
-
-
-        getCountriesData();
-  },[]);
-  const onCountryChange = async (event)=> {
+    getCountriesData();
+  }, []);
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    console.log(countryCode);
-   setCountry(countryCode)
+    
+    const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+    await fetch(url).then(res => res.json()).then(data=> {
+        setCountry(countryCode);
+        setCountryInfo(data);
+    })
+ 
   };
 
   return (
     <div className="app">
-      <div>
+      <div className="app__left">
         <div className="app__header">
           <h1>COVID-19 TRACKER </h1>
           <FormControl className="app__dropdown">
@@ -53,20 +64,33 @@ function App() {
           </FormControl>
         </div>
         <div className="app__stats">
-          <InfoBox title="Coronavirus Cases" cases={123} total={2000} />
-          <InfoBox title="Recover cases" cases={1234} total={100000} />
-          <InfoBox title="Deaths" cases={11234} total={4000} />
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recover cases"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
         </div>
 
-        {/* Table */}
-        {/* Graph */}
         {/* Map */}
         <Map />
       </div>
 
-      <div>
-        
-      </div>
+      <Card className="app__right">
+        <h3> Live Cases by Country </h3>
+        {/* Table */}
+        <h3> Worldwide new cases </h3>
+        {/* Graph */}
+      </Card>
     </div>
   );
 }
